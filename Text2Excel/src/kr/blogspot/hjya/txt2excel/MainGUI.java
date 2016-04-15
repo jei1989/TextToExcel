4af1487dcd0b3e70098559e896d421dd9e48917d
Administrator <Administrator@119.196.231.201>
2016-04-16 05:27:47 +0900

    ver0.1

---
+++ Text2Excel/src/kr/blogspot/hjya/txt2excel/MainGUI.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package kr.blogspot.hjya.txt2excel;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MainGUI {
+
+	/**
+	 * 
+	 */
+	public MainGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainGUI();
+	}
+
+}
